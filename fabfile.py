57ab5a659356cf8388e79092e184a27e45efbaf1
Jeff Soule <jeff@soule.me>
2016-07-08 08:49:58 -0700

    debugging

--- fabfile.py
+++ fabfile.py
@@ -46,11 +46,14 @@ def kernelReport():
     """Report all running kernel versions"""
     with hide('commands'):
         env.parallel = True
-        result = run("uname -r")
+        result = run("uname -r", capture=True)
         redhat = run("cat /etc/redhat-release")
         uptime = run("uptime")
         kernels = run("rpm -q kernel")
         numkern = len(kernels.split('\n'))
+        if(result.stderr != ""):
+            error = "On %s: %s" %(result, result.stderr)
+            print error
         print "Result Return Code: %s" % result.return_code
         print "<font color=white>%s: </font><font color=yellow>%s</font>" % (env.host, result)
         print "<font color=white>%s: </font><font color=yellow>%s</font>" % (env.host, redhat)
