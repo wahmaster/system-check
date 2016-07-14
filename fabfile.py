6c2361222503258daa4d067088e0844400da1f39
Jeff Soule <jeff@soule.me>
2016-07-14 12:55:07 -0700

    Added needs patching to output

--- fabfile.py
+++ fabfile.py
@@ -34,7 +34,7 @@ def kernelReport():
         redhat = run("cat /etc/redhat-release")
         kernels = run("rpm -q kernel")
         numkern = len(kernels.split('\n'))
-        checkpatch = run("yum check-update --disablerepo='*artifactory' %s | awk 'p;/^$/{p=1}'" % (env.excludes), pty=True)
+        checkpatch = run("yum check-update --disablerepo='*artifactory' %s " % (env.excludes), pty=True)
         if checkpatch.return_code == 100:
             needspatch = "True"
         elif checkpatch.return_code == 0:
